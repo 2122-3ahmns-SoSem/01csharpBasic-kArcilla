eb2b66947039b5e721b29c3b3d8fefe5c4ef5830
Kaiaia-doesnotexist <100835211+Kaiaia-doesnotexist@users.noreply.github.com>
2022-03-10 10:16:13 +0100

    Add files via upload

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        HelloWorld();
+    }
+
+    public void HelloWorld()
+    {
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
